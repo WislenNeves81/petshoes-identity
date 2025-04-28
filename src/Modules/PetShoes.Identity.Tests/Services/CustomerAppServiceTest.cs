8adc9d3fd99ba7000e1caae6da71669879371a09
WislenNeves81 <wislen.neves@gmail.com>
2025-04-28 09:10:37 -0300

    [UPDATE] - Inclusão de testes.

---
+++ src/Modules/PetShoes.Identity.Tests/Services/CustomerAppServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using Bogus;
+using FluentAssertions;
+using NSubstitute;
+using PetShoes.Identity.Application.AppCustomer;
+using PetShoes.Identity.Application.AppCustomer.ViewModel;
+using PetShoes.Identity.Domain.Interfaces;
+using PetShoes.Identity.Tests.Services.Generate;
+
+namespace PetShoes.Identity.Tests.Services
+{
+    public class CustomerAppServiceTest
+    {
+
+        private ICustomerRepository _customerRepository;
+        private CustomerAppService _customerAppService;
+        private Faker _faker;
+
+        private const int defaultReceived = 1;
+
+        public CustomerAppServiceTest()
+        {
+            _customerRepository = Substitute.For<ICustomerRepository>();
+            _customerAppService = new CustomerAppService(_customerRepository);
+        }
+
+
+        [Fact]
+        public async Task GetCustomerByIdAsync_When_Returns_Value()
+        {
+            //Arrange
+            var customerId = _faker.Random.Guid();
+            var customer = GenerateFakerCustomer.CreateCustomerObject(customerId);
+            _customerRepository.GetCustomerByIdAsync(Arg.Any<Guid>()).Returns(customer);
+
+            //Act
+            var result = await _customerAppService.GetCustomerByIdAsync(customerId);
+
+            //Assert
+            result.Should().BeOfType<CustomerViewModel>();
+
+            await _customerRepository
+                    .Received(defaultReceived)
+                    .GetCustomerByIdAsync(Arg.Any<Guid>());
+        }
+    }
+}
---
+++ src/Modules/PetShoes.Identity.Tests/Services/Generate/GenerateFakerCustomer.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using PetShoes.Identity.Repositories.Repository;
+
+namespace PetShoes.Identity.Tests.Services.Generate
+{
+    internal class GenerateFakerCustomer
+    {
+        private const string UtfType = "pt_BR";
+        private const int IntEight = 8;
+        public static Customer CreateCustomerObject(Guid id)
+        {
+            var customer = new Faker<Customer>(UtfType)
+                                        .StrictMode(true)
+                                        .RuleFor(c => c.Id, id)
+                                        .RuleFor(c => c.Name, faker => faker.Person.FullName)
+                                        .RuleFor(c => c.Email, faker => faker.Person.Email)
+                                        .RuleFor(c => c.Identification, faker => faker.Person.Cpf())
+                                        .RuleFor(c => c.Phone, faker => faker.Person.Phone)
+                                        .RuleFor(c => c.City, faker => faker.Person.Address.City)
+                                        .RuleFor(c => c.UF, faker => faker.Person.Address.State)
+                                        .RuleFor(c => c.Password, faker => faker.Random.AlphaNumeric(IntEight))
+                                        .RuleFor(c => c.Active, true)
+                                        .RuleFor(c => c.Admin, faker => faker.Random.Bool())
+                                        .Generate();
+            return customer;
+        }
+    }
+}
